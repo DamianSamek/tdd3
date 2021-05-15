00d0d2d269bbee1ac751654e42584c71cefa3fec
Damian Samek <damiansamek95@gmail.com>
2021-05-15 11:22:01 +0200

    method extension

--- src/tdd3/RectangularTriangleChecker.java
+++ src/tdd3/RectangularTriangleChecker.java
@@ -3,6 +3,9 @@ package tdd3;
 public class RectangularTriangleChecker {
 
 	public static boolean canBuildRectangularTriangle(int i, int j, int k) {
+		if (i <= 0 || j <= 0 || k <= 0) {
+			throw new ArithmeticException();
+		}
 		return Math.pow(i, 2) + Math.pow(j, 2) == Math.pow(k, 2);
 	}
 
