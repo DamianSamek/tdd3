be4951df2a8091faf06c437178bc5f1eec94050a
Damian Samek <damiansamek95@gmail.com>
2021-05-15 11:09:41 +0200

    second case

--- src/tdd3/RectangularTriangleTest.java
+++ src/tdd3/RectangularTriangleTest.java
@@ -1,5 +1,6 @@
 package tdd3;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -9,5 +10,10 @@ public class RectangularTriangleTest {
 	@Test
 	public void testWhenAllParametersAreOk() {
 		assertTrue(RectangularTriangleChecker.canBuildRectangularTriangle(3,4,5));
+	}
+	
+	@Test
+	public void testWhenAllParametersAreNotOk() {
+		assertFalse(RectangularTriangleChecker.canBuildRectangularTriangle(3,4,9));
 	}
 }
