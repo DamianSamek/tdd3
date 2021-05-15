02e7cd23e3f4083c2b8946c662855f29b8f741b4
Damian Samek <damiansamek95@gmail.com>
2021-05-15 11:21:34 +0200

    method extension

--- src/tdd3/RectangularTriangleTest.java
+++ src/tdd3/RectangularTriangleTest.java
@@ -2,6 +2,7 @@ package tdd3;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.Test;
 
@@ -15,5 +16,10 @@ public class RectangularTriangleTest {
 	@Test
 	public void testWhenAllParametersAreNotOk() {
 		assertFalse(RectangularTriangleChecker.canBuildRectangularTriangle(3,4,9));
+	}
+	
+	@Test
+	public void testWhenAtLeastOneParameterIsZeroOrLessThanZero() {
+		assertThrows(ArithmeticException.class, () -> RectangularTriangleChecker.canBuildRectangularTriangle(0, 2, 5));
 	}
 }
