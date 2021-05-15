44bebcda93eb0b54dbf75af9fe3e074c0f8bfefe
Damian Samek <damiansamek95@gmail.com>
2021-05-15 11:08:48 +0200

    refactor

--- src/tdd3/RectangularTriangleChecker.java
+++ src/tdd3/RectangularTriangleChecker.java
@@ -3,10 +3,7 @@ package tdd3;
 public class RectangularTriangleChecker {
 
 	public static boolean canBuildRectangularTriangle(int i, int j, int k) {
-		if(Math.pow(i, 2) + Math.pow(j, 2) == Math.pow(k, 2)) {
-			return true;
-		}
-		return false;
+		return Math.pow(i, 2) + Math.pow(j, 2) == Math.pow(k, 2);
 	}
 
 }
